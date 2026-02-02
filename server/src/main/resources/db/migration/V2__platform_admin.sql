f7d1549bb4d87681118292ba12ef49c84c31980c
Ciwot18 <ciro.vavotici@gmail.com>
2026-02-02 11:38:56 +0100

    Added user into DB for Platform user

---
+++ server/src/main/resources/db/migration/V2__platform_admin.sql
@@ -0,0 +1,13 @@
+INSERT INTO companies (name, vat_number)
+VALUES ('__PLATFORM__', NULL);
+
+INSERT INTO users (company_id, email, password_hash, role, enabled)
+VALUES (
+           (SELECT id FROM companies WHERE name='__PLATFORM__'),
+           'platform.admin@easyinvoice.com',
+           '$2a$12$3zC7J6G1IJQcrCQn9OlFNu9sUhlSlbaGtZgHAnDYvazbGOBUYtyy.',
+           'PLATFORM_ADMIN',
+           TRUE
+       );
+
+-- Admin123!!
