aba8601bea510e4c2610094ccd0e94b597ce63de
Ciwot18 <ciro.vavotici@gmail.com>
2026-02-02 12:07:09 +0100

    Adapted Security to support requests to /auth

--- server/src/main/java/com/kernith/easyinvoice/config/SecurityConfig.java
+++ server/src/main/java/com/kernith/easyinvoice/config/SecurityConfig.java
@@ -3,21 +3,21 @@ package com.kernith.easyinvoice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
                 .csrf(csrf -> csrf.disable())
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/test/**").permitAll()
+                        .requestMatchers("/auth/**", "/error").permitAll()
                         .anyRequest().authenticated()
                 )
-                .formLogin(form -> form.disable());
-
-        return http.build();
+                .build();
     }
 }
