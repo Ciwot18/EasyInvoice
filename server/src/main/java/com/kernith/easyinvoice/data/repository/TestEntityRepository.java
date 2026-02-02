159a7ebaaf8c027aa40d5d28cb3780d7f3bf2fe4
Ciwot18 <ciro.vavotici@gmail.com>
2026-02-02 14:57:47 +0100

    Removed 3 TEST files

--- server/src/main/java/com/kernith/easyinvoice/data/repository/TestEntityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.kernith.easyinvoice.data.repository;
-
-import com.kernith.easyinvoice.data.model.TestEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestEntityRepository extends JpaRepository<TestEntity, Long> {
-}
