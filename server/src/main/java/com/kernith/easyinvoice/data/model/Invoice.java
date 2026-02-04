2dff6a6150b088c9380563ae3573ec8034809c6e
Ciro Vavotici <ciro.vavotici@gmail.com>
2026-02-04 20:31:30 +0100

    Added Transient Notation for state field

--- server/src/main/java/com/kernith/easyinvoice/data/model/Invoice.java
+++ server/src/main/java/com/kernith/easyinvoice/data/model/Invoice.java
@@ -2,21 +2,8 @@ package com.kernith.easyinvoice.data.model;
 
 import com.kernith.easyinvoice.data.model.state.invoice.InvoiceState;
 import com.kernith.easyinvoice.data.model.state.invoice.InvoiceStateFactory;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Index;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.PostLoad;
-import jakarta.persistence.Table;
-import jakarta.persistence.UniqueConstraint;
+import jakarta.persistence.*;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -102,6 +89,7 @@ public class Invoice {
     @Column(name = "updated_at", nullable = false, updatable = false, insertable = false)
     private LocalDateTime updatedAt;
 
+    @Transient
     private InvoiceState state;
 
     public Invoice() {}
