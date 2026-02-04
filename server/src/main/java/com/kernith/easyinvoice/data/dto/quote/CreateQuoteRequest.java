66240760486aca4138104b52a0fc194e7f7b98e3
Ciro Vavotici <ciro.vavotici@gmail.com>
2026-02-04 03:42:54 +0100

    Added items to Request for creation of Quote

--- server/src/main/java/com/kernith/easyinvoice/data/dto/quote/CreateQuoteRequest.java
+++ server/src/main/java/com/kernith/easyinvoice/data/dto/quote/CreateQuoteRequest.java
@@ -1,8 +1,11 @@
 package com.kernith.easyinvoice.data.dto.quote;
 
+import com.kernith.easyinvoice.data.dto.quoteitem.CreateQuoteItemRequest;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import java.time.LocalDate;
+import java.util.List;
 
 public record CreateQuoteRequest(
         @NotNull Long customerId,
@@ -10,5 +13,6 @@ public record CreateQuoteRequest(
         @Size(max = 2000) String notes,
         LocalDate issueDate,
         LocalDate validUntil,
-        @Size(max = 3) String currency
+        @Size(max = 3) String currency,
+        @NotNull @Size(min = 1) List<@Valid CreateQuoteItemRequest> items
 ) {}
