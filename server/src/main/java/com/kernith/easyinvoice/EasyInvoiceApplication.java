eaf3c8561e5358d71d2d3f0bdaeac31ca2edb18b
Ciro Vavotici <ciro.vavotici@gmail.com>
2026-02-05 17:12:50 +0100

    Explicited Scheduling

--- server/src/main/java/com/kernith/easyinvoice/EasyInvoiceApplication.java
+++ server/src/main/java/com/kernith/easyinvoice/EasyInvoiceApplication.java
@@ -2,8 +2,10 @@ package com.kernith.easyinvoice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class EasyInvoiceApplication {
 
     public static void main(String[] args) {
