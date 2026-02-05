eaacc282068b2a2d0f73e66683310e56ecad5c8e
Ciro Vavotici <ciro.vavotici@gmail.com>
2026-02-05 19:02:43 +0100

    Added OpenAPI to have Swagger UI with all end-points

--- server/src/main/java/com/kernith/easyinvoice/EasyInvoiceApplication.java
+++ server/src/main/java/com/kernith/easyinvoice/EasyInvoiceApplication.java
@@ -1,9 +1,14 @@
 package com.kernith.easyinvoice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@OpenAPIDefinition(
+        info = @Info(title = "EasyInvoice API", version = "1.0")
+)
 @SpringBootApplication
 @EnableScheduling
 public class EasyInvoiceApplication {
