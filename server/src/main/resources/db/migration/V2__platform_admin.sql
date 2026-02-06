51f9561c6db6c2b27f34a2d82ea69d95ec29d905
Ciro Vavotici <ciro.vavotici@gmail.com>
2026-02-06 20:27:06 +0100

    Added Name Field

--- server/src/main/resources/db/migration/V2__platform_admin.sql
+++ server/src/main/resources/db/migration/V2__platform_admin.sql
@@ -1,9 +1,10 @@
 INSERT INTO companies (name, vat_number)
 VALUES ('__PLATFORM__', NULL);
 
-INSERT INTO users (company_id, email, password_hash, role, enabled)
+INSERT INTO users (company_id, name, email, password_hash, role, enabled)
 VALUES (
            (SELECT id FROM companies WHERE name='__PLATFORM__'),
+           'Giuseppe Garibaldi',
            'platform.admin@easyinvoice.com',
            '$2a$12$3zC7J6G1IJQcrCQn9OlFNu9sUhlSlbaGtZgHAnDYvazbGOBUYtyy.',
            'PLATFORM_ADMIN',
